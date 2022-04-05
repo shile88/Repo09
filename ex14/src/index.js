1cc4f678cfd585ec2c9c4cdc4bbb8424077d5945
shile88 <milos.ivanis@gmail.com>
2022-04-05 17:25:03 +0200

    Ternary operator

---
+++ ex14/src/index.js
@@ -0,0 +1,11 @@
+function checkSign(num){
+    return (num == 0) ? "zero"
+    : (num > 0) ? "positive"
+    : "negative";
+}
+
+console.log(checkSign(10));
+console.log(checkSign(-12));
+console.log(checkSign(0));
+
+module.exports = checkSign;
