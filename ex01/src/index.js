3035e90fa6ce9f69bb6f668218c1be0112e9247f
shile88 <milos.ivanis@gmail.com>
2022-04-05 12:26:00 +0200

    Boolean function

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function myTrueBoolean(){
+    var num = 2;
+    if(num = 2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(myTrueBoolean());
+
+module.exports = myTrueBoolean;
