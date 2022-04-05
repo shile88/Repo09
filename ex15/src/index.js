54ea31ac56a5390dd0a4c36bfd5d3eae1a809082
shile88 <milos.ivanis@gmail.com>
2022-04-05 18:39:38 +0200

    Switch statements

---
+++ ex15/src/index.js
@@ -0,0 +1,27 @@
+
+function switchCase(letter) {
+    var animals = letter;
+
+  switch (animals) {
+    case "a":
+      animals = "antelope";
+      break;
+    case "b":
+      animals = "bird";
+      break;
+    case "c":
+      animals = "cat";
+      break;
+    default:
+      animals = "stuff";
+    }
+      return animals;
+}
+
+console.log(switchCase("a"));
+console.log(switchCase("b"));
+console.log(switchCase("c"));
+console.log(switchCase("d"));
+console.log(switchCase(4));
+
+module.exports = switchCase;
