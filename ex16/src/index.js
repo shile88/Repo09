2d8a0c8a4c5e1acdb6d8140512ab515308b22d95
shile88 <milos.ivanis@gmail.com>
2022-04-11 11:40:00 +0200

    need to finish

---
+++ ex16/src/index.js
@@ -0,0 +1,11 @@
+fucntion switchCaseSequential(num){
+    var answer = 
+
+    switch(answer){
+        case "1-3":
+        case "4-6":
+        case "7-9":
+            answer = "" 
+    }
+    
+}
