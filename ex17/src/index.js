a79dbbd2829eb6eb90fd2e49641bd161d4d0d8ae
shile88 <milos.ivanis@gmail.com>
2022-04-11 11:38:57 +0200

    need to finish

---
+++ ex17/src/index.js
@@ -0,0 +1,10 @@
+function inverseWhite(){
+    var i = 5;
+    while (i >= 0){
+        var fiveNumbers = "i--";
+        console.log(fiveNumbers);
+    }
+}
+
+console.log(inverseWhite());
+module.exports = inverseWhite;
